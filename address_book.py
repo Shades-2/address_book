3758996cf740bf4cc752531f6bccb1ab0aa34d91
Sonny Hartney <sonnyhartney@hotmail.com>
2021-04-04 15:55:27 +0100

    Adress book code

---
+++ address_book.py
@@ -0,0 +1,67 @@
+import pickle
+contactsfile = 'contacts.data'
+
+
+class Contact:
+    def __init__(self, name, email_address, phone_number):
+        self.name = name
+        self.email_address = email_address
+        self.phone_number = phone_number
+
+    def show(self):
+        """Shows details."""
+        print("Name = {}, Email Address = {}, Phone number = {}".format(self.name,
+                                                                        self.email_address, self.phone_number))
+
+
+class AddressBook:
+    def __init__(self):
+        self.contacts = {}
+
+    def add_contact(self, contact, object):
+        """add contact to address book, contact is the key you will use to find the contact and
+         the value is the contact object"""
+        self.contacts[contact] = object
+
+        # Name of the file that will store the address book object
+        # Write to the file
+        f = open(contactsfile, 'wb')
+        # Dump to the file
+        pickle.dump(self.contacts, f)
+        f.close()
+        print("Added Contact", contact)
+
+    def remove_contact(self, contact):
+        """Removes contact"""
+        if contact in self.contacts:
+            del self.contacts[contact]
+
+            # Name of the file that will store the address book object
+            # Write to the file
+            f = open(contactsfile, 'wb')
+            # Dump to the file
+            pickle.dump(self.contacts, f)
+            f.close()
+            print("Removed Contact", contact)
+
+        else:
+            print("Could not delete contact, contact does not exist.")
+
+    def search_contact(self, contact):
+        """searches the dictionary for the key passed in as an arg and returns the Contact object's detail's"""
+        if contact in self.contacts:
+            print(self.contacts.get(contact).show())
+
+        else:
+            print("Contact not found.")
+
+    def list_contacts(self):
+        """Lists all contacts using a for in loop"""
+        for k in self.contacts.keys():
+            print(k)
+
+
+jamie = Contact("Jamie", "jamie@hotmail.com", "01273")
+ab = AddressBook()
+ab.add_contact("Jamie", jamie)
+
