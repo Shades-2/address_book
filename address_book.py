cb0979bb5c654dbf3e44e9122c9b9c10445e8c0c
Sonny Hartney <sonnyhartney@hotmail.com>
2021-04-04 16:47:07 +0100

    Some comments for code change and added .gitignore

--- address_book.py
+++ address_book.py
@@ -1,6 +1,7 @@
 import pickle
-contactsfile = 'contacts.data'
 
+# Can we always save in project root
+contactsfile = 'contacts.data'
 
 class Contact:
     def __init__(self, name, email_address, phone_number):
@@ -10,11 +11,12 @@ class Contact:
 
     def show(self):
         """Shows details."""
-        print(f'Name = {self.name}, Email Address = {self.email_address}, Phone number = {self.phone_number}'
+        print(f'Name = {self.name}, Email Address = {self.email_address}, Phone number = {self.phone_number}')
 
 
 class AddressBook:
     def __init__(self):
+        # should be private self._contracts encapsulation of data.
         self.contacts = {}
 
     def add_contact(self, contact, object):
@@ -26,6 +28,8 @@ class AddressBook:
         # Write to the file
         f = open(contactsfile, 'wb')
         # Dump to the file
+
+        # Can we save just the data. Preferable in json format (use json lib ie import json)??
         pickle.dump(self.contacts, f)
         f.close()
         print("Added Contact", contact)
@@ -60,6 +64,8 @@ class AddressBook:
             print(k)
 
 
+# if __name__ == '__main__':
+# Or put in function .. def test_adress_book():
 jamie = Contact("Jamie", "jamie@hotmail.com", "01273")
 ab = AddressBook()
 ab.add_contact("Jamie", jamie)
