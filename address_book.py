2caaab0f6f3ac186e2f3aa4ed83b265da0a4524b
Shades-2 <sonnyhartney@hotmail.com>
2021-04-20 16:35:02 +0100

    fixed all things in comments

--- address_book.py
+++ address_book.py
@@ -15,56 +15,41 @@ class Contact:
 
 
 class AddressBook:
-    '''Write a name for your file as the argument for the AddressBook you are instantiating. 
-       This will be the file where the contacts are saved. .json will be added to the filename automatically.'''
-    # dont need to duplicate name
+    """Write a name for your file as the argument for the AddressBook you are instantiating. 
+       This will be the file where the contacts are saved. .json will be added to the filename automatically."""
 
-    def __init__(self, contactsfile):
+    def __init__(self, contacts_file):
+
         # This is where the data will be saved
-        self.contactsfile = contactsfile
+        self.contacts_file = contacts_file
         self._contacts = {}
+
         # if the file already exists, load the data in into the _contacts dict by iterating through the file
-        if Path(self.contactsfile).exists == True:
-            with open(self.contactsfile) as f:
+        if Path(self.contacts_file).exists == True:
+            with open(self.contacts_file) as f:
                 data = json.load(f)
 
             for name, details in data.items():
                 self._contacts[name] = Contact(
                     name, details['email_address'], details['phone_number'])
 
-        elif Path(self.contactsfile).exists == False:
-            open(self.contactsfile)
-
-    def add_contact(self, contact, object):
+    def add_contact(self, contact):
         """add contact to address book, contact is the key you will use to find the contact and
          the value is the contact object"""
-        self._contacts[contact] = vars(object)
-
-        # Name of the file that will store the address book object
-        # Write to the file
-        with open(self.contactsfile, 'w') as f:
-            # Dump to the file
-            json.dump(self._contacts, f, indent=2)
-
-        print("Added Contact", contact)
+        self._contacts[contact.name] = vars(contact)
+        print(f"Added Contact {contact.name}")
 
     def remove_contact(self, contact):
         """Removes contact"""
         if contact in self._contacts:
             del self._contacts[contact]
-
-            # Name of the file that will store the address book object
-            # Write to the file
-            with open(self.contactsfile, 'w') as f:
-                # Dump to the file
-                json.dump(self._contacts, f)
             print("Removed Contact", contact)
 
         else:
             print("Could not delete contact, contact does not exist.")
 
     def search_contact(self, contact):
-        """searches the dictionary for the key passed in as an arg and returns the Contact object's detail's"""
+        """ Searches the dictionary for the key passed in as an arg and returns the Contact object's detail's. """
         if contact in self._contacts:
             print(self._contacts.get(contact))
 
@@ -72,6 +57,13 @@ class AddressBook:
             print("Contact not found.")
 
     def list_contacts(self):
-        """Lists all contacts using a for in loop"""
+        """ Lists all contacts using a for in loop. """
         for ls in self._contacts.keys():
             print(ls)
+
+    def save(self):
+        """ Saves _contacts file to disk. """
+        with open(self.contacts_file, 'w') as f:
+            # Dump to the file
+            json.dump(self._contacts, f, indent=2)
+            print("Contact's saved to file.")
--- test_address_book.py
+++ test_address_book.py
@@ -7,26 +7,17 @@ class TestAddressBook(unittest.TestCase):
     def setUp(self):
         self.john = address_book.Contact("John", "email.address", "123")
         self.ab = address_book.AddressBook('contact.json')
-        self.ab.add_contact('John', self.john)
-
-    def tearDown(self):
-        Path(self.ab.contactsfile).unlink()
-
-    def test_show(self):
-        self.assertEqual(self.john.name, "John")
-        self.assertEqual(self.john.email_address, "email.address")
-        self.assertEqual(self.john.phone_number, "123")
+        self.ab.add_contact(self.john)
 
     def test_add_contact(self):
+        jack = address_book.Contact('Jack', 'jack@email.com', 234)
+        self.ab.add_contact(jack)
         self.assertIn('John', self.ab._contacts)
+        self.assertIn('Jack', self.ab._contacts)
 
     def test_remove_contact(self):
         self.ab.remove_contact('John')
         self.assertNotIn('John', self.ab._contacts)
-
-    def test_the_contactsfile(self):
-        assert Path(self.ab.contactsfile).exists
-        assert (self.ab._contacts) == (self.ab._contacts)
 
     def test_search_contact(self):
         self.assertEqual(self.ab._contacts.get('John'), {
@@ -34,6 +25,21 @@ class TestAddressBook(unittest.TestCase):
 
     def test_list_contacts(self):
         self.ab.list_contacts() == ('John')
+
+    def test_save(self):
+        self.ab.save
+        assert Path(self.ab.contacts_file).exists
+        
+
+
+class TestContact(unittest.TestCase):
+    def setUp(self):
+        self.john = address_book.Contact("John", "email.address", "123")
+
+    def test_show(self):
+        self.assertEqual(self.john.name, "John")
+        self.assertEqual(self.john.email_address, "email.address")
+        self.assertEqual(self.john.phone_number, "123")
 
 
 if __name__ == '__main__':
