0a33aae177ae824629b4bc9196d4a37247c9734d
Shades-2 <sonnyhartney@hotmail.com>
2021-05-03 16:49:32 +0100

    removed all the print statements and fixed stuff in comments

--- address_book.py
+++ address_book.py
@@ -36,34 +36,33 @@ class AddressBook:
     def add_contact(self, contact):
         """add contact to address book, contact is the key you will use to find the contact and
          the value is the contact object"""
-        self._contacts[contact.name] = vars(contact)
-        print(f"Added Contact {contact.name}")
+        self._contacts[contact.name] = {
+            'email_address': contact.email_address, 'phone_number': contact.phone_number}
 
     def remove_contact(self, contact):
         """Removes contact"""
         if contact in self._contacts:
             del self._contacts[contact]
-            print("Removed Contact", contact)
-
+            
         else:
-            print("Could not delete contact, contact does not exist.")
+            raise Exception("Contact doesn't exist")
 
     def search_contact(self, contact):
         """ Searches the dictionary for the key passed in as an arg and returns the Contact object's detail's. """
         if contact in self._contacts:
-            print(self._contacts.get(contact))
+            return None
 
         else:
-            print("Contact not found.")
+            raise Exception("Contact doesn't exist")
 
     def list_contacts(self):
         """ Lists all contacts using a for in loop. """
         for ls in self._contacts.keys():
-            print(ls)
+            return ls
 
     def save(self):
         """ Saves _contacts file to disk. """
         with open(self.contacts_file, 'w') as f:
             # Dump to the file
             json.dump(self._contacts, f, indent=2)
-            print("Contact's saved to file.")
+
